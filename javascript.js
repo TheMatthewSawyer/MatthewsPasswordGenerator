71ce3fdf13c724a5d5f3b9493bd2afdf5c3c1f86
Matthew Sawyer <57121937+matthewleonardsawyer@users.noreply.github.com>
2019-12-16 04:11:53 -0500

    Added javascript for generating the password and copying it

---
+++ javascript.js
@@ -0,0 +1,54 @@
+var specialCharacters = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+function inputValidate() {
+    var passwordLength = document.getElementById('passwordLengthField').value;
+    if (passwordLength === "" || passwordLength === " " || passwordLength === "  " || passwordLength === "   ") {
+        alert("Do not leave password length field empty.");
+        return;
+    }
+    if (/[^0-9]/.test(passwordLength)){
+        alert("Only enter numbers in the password length field");
+        return;
+    }
+    passwordLength = parseInt(passwordLength);
+    if (passwordLength > 128 || passwordLength < 8) {
+        alert("Make sure your number is between 8 and 128");
+        return;
+    }
+    generatePassword(passwordLength);
+}
+
+function generatePassword(passwordLength) {
+    var userSelection = [];
+    var newPassword = "";
+    if (document.getElementById('specialCharactersYes').checked) {
+        userSelection = userSelection.concat(specialCharacters);
+    }
+    if (document.getElementById('numbersYes').checked) {
+        userSelection = userSelection.concat(numbers);
+    }
+    if (document.getElementById('lowercaseYes').checked) {
+        userSelection = userSelection.concat(lowercaseLetters);
+    }
+    if (document.getElementById('uppercaseYes').checked) {
+        userSelection = userSelection.concat(uppercaseLetters);
+    }
+    if (userSelection.length < 1) {
+        alert("Please select at least one option")
+        return;
+    }
+    for (var i = 0; i < passwordLength; i++) {
+        newPassword += userSelection[Math.floor(Math.random() * userSelection.length)];
+    }
+    document.getElementById("password").innerHTML = newPassword;
+}
+
+function copyNewPass() {
+    document.querySelector("textarea").select();
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+    return;
+}
